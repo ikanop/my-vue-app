daa0103cc1f61233cd74f5441cfbf12a271cd74a
ikanop <adrianulrix@gmail.com>
2025-08-26 13:17:17 +0200

    changed list

--- src/components/todoList.vue
+++ src/components/todoList.vue
@@ -21,10 +21,8 @@ function removeTodo(todo) {
     <input v-model="newTodo" required placeholder="New todo" />
     <button style="cursor: pointer;">Add</button>
   </form>
-  <ul>
-    <li v-for="todo in todos" :key="todo.id">
-      {{ todo.id }} {{ todo.text }}
-      <button @click="removeTodo(todo)" style="background-color: transparent; border-color: transparent; color: red; cursor: pointer;">X</button>
-    </li>
+  <ul v-for="todo in todos" :key="todo.id">
+    {{ todo.id }} {{ todo.text }}
+    <button @click="removeTodo(todo)" style="background-color: transparent; border-color: transparent; color: red; cursor: pointer;">X</button>
   </ul>
 </template>
